f5ea8a2e2ea4022e90b8891bc9a1475d40ef20b8
dattanchu <dattanchu@1fd97272-c059-11de-8907-fd8f2f62d6ab>
2009-11-28 15:52:56 +0000

    Code that has working HoughCircles, although not good.

--- trunk/src/iris_main.cxx
+++ trunk/src/iris_main.cxx
@@ -7,8 +7,12 @@ using namespace std;
 using namespace cv;
 
 int main(int argc, char ** argv){
+
+	// PARAMETERS
 	const string cascade_name = "haarcascade_lefteye_2splits.xml" ;
-	const int max_frame = 100;
+	const int max_frame = 30;
+	const double edge_threshold = 130;
+	const double hough_circle_min_distance = 10;
 
 	int count_frame = 0;
 
@@ -22,7 +26,10 @@ int main(int argc, char ** argv){
 	Mat temp_frame; // temporary store the currently processed frame
 	Mat temp_out_frame;
 	Mat cropped_frame;
+	Mat cropped_frame_gray;
+	Mat edges;
 	vector<Rect> detected_objects;
+	vector<Vec3f> circles; // hough circles
 
 	char * cropped_frame_window = "Cropped Frame";
 	namedWindow(cropped_frame_window, CV_WINDOW_AUTOSIZE);
@@ -43,7 +50,8 @@ int main(int argc, char ** argv){
 	VideoWriter video_writer("out_frame.avi", CV_FOURCC('M','P','E','G'), 30, temp_frame.size(), true);
 
 	// while we can still capture
-	while (count_frame<max_frame && capture.grab()){
+	//while (count_frame<max_frame && capture.grab()){
+	while (capture.grab()){
 		capture.retrieve(temp_frame);
 
 		// Perform detection
@@ -61,16 +69,36 @@ int main(int argc, char ** argv){
 		if (detected_objects.size() > 0 ){
 			rectangle(temp_out_frame, detected_objects[0].tl(), detected_objects[0].br(), CV_RGB(255,0,0));
 			cropped_frame = Mat(temp_frame, detected_objects[0]);
-		}
 
-//		imshow(cropped_frame_window, cropped_frame);
-//		waitKey(0);
+			// only use gray image (since Canny can only handle gray)
+			cvtColor(cropped_frame, cropped_frame_gray, CV_RGB2GRAY);
+
+			// canny edge detection
+			edges = Mat(cropped_frame_gray.size(),cropped_frame_gray.type());
+			Canny(cropped_frame_gray, edges, edge_threshold, edge_threshold * 3);
+
+			// hough circle
+			HoughCircles(edges, circles, CV_HOUGH_GRADIENT, 1, hough_circle_min_distance, 1, 10);
+
+			for(size_t i=0; i <circles.size();i++)
+			{
+				Point center(cvRound(circles[i][0]),cvRound(circles[i][1]));
+				int radius = cvRound(circles[i][2]);
+				//draw the circle center
+				circle(cropped_frame, center , 3 , CV_RGB(255,0,0),1,8,0);
+				//draw the circle out line
+				circle(cropped_frame, center, radius, CV_RGB(0,0,255),1,8,0);
+			}
+		}
 
 		printf(".");
 		
 		char out_filename[200];
+		char out_iris_filename[200];
 		sprintf(out_filename, "left_eye_%d.png", count_frame);
-		cvSaveImage(out_filename, &IplImage(cropped_frame));
+		sprintf(out_iris_filename, "left_iris_%d.png", count_frame);
+		cvSaveImage(out_filename, &IplImage(edges));
+		cvSaveImage(out_iris_filename, &IplImage(cropped_frame));
 		video_writer << temp_out_frame;
 		
 		// NOTE: for now, don't process the entire thing yet
